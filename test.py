6213061bb8580e76e13876dcb14a5411a7478865
sarthakjadhav21 <152524492+sarthakjadhav21@users.noreply.github.com>
2023-11-30 22:43:21 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+# import webapp2
+
+# class MainPage(webapp2.RequestHandler):
+#     def get(self):
+#         self.response.write("Namaste World!")
+
+# app = webapp2.WSGIApplication([('/')], debug=True)
+
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Namaste Soham.!")
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
